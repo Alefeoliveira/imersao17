161c0a1ae8e9cbf68cdc331ba23bc438602b090a
Alefe Oliveira <alefe.oliveira@logiquesistemas.com.br>
2024-01-31 13:32:05 -0300

    ajustes

--- internal/service/product_service.go
+++ internal/service/product_service.go
@@ -1,6 +1,9 @@
 package service
 
-import "github.com/Alefeoliveira/imersao17/goapi/internal/db"
+import (
+	"github.com/Alefeoliveira/imersao17/goapi/internal/db"
+	"github.com/Alefeoliveira/imersao17/goapi/internal/entity"
+)
 
 type ProductService struct {
 	ProductDB db.ProductDB
@@ -8,4 +11,29 @@ type ProductService struct {
 
 func NewProductService(productDb db.ProductDB) *ProductService {
 	return &ProductService{ProductDB: productDb}
+}
+
+func (cs *ProductService) GetProducts() ([]*entity.Product, error) {
+	products, err := cs.ProductDB.GetProducts()
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
+}
+
+func (cs *ProductService) CreateProduct(name, description, category_id, image_url string, price float64) (*entity.Product, error) {
+	product := entity.NewProduct(name, description, category_id, image_url, price)
+	_, err := cs.ProductDB.CreateProduct(product)
+	if err != nil {
+		return nil, err
+	}
+	return product, nil
+}
+
+func (cs *ProductService) GetProductByCategoryID(id string) ([]*entity.Product, error) {
+	products, err := cs.ProductDB.GetProductByCategoryID(id)
+	if err != nil {
+		return nil, err
+	}
+	return products, nil
 }
